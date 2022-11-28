48c0f8705ca3e9bfebe1c5f2e0cf51ce58d21c97
imane <92234199+imaneeoublid@users.noreply.github.com>
2022-11-28 09:38:36 +0100

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,33 @@
+public class triangle {
+
+    
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 1; i <= 10; i++) {
+            if (i < 10)
+                System.out.print(i + " ");
+            else
+                System.out.print(i);
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("\n");
+
+        for (i = 1; i <= 10; i++) {
+            if (i < 10)
+                System.out.print(i + " ");
+            else
+                System.out.print(i);
+            for (j = 10; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
+
